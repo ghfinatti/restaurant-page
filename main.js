a5c461d43c5363ef0e88cb41b21ab0b0b600944d
Gabriel Finatti <ghfinatti3@gmail.com>
2021-09-09 18:57:49 -0300

    nav bar added with javascript

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("test");
+(()=>{"use strict";const e=(()=>{const e=document.querySelector("#content"),t=document.createElement("div");return{createHeader:()=>{t.classList.add("nav-bar"),e.appendChild(t)},createNavBtns:()=>{const e=document.createElement("div"),n=document.createElement("div"),a=document.createElement("div");t.appendChild(e),t.appendChild(n),t.appendChild(a),e.textContent="Home",n.textContent="Menu",a.textContent="Contact"}}})();e.createHeader(),e.createNavBtns()})();
